62aca62bc0b9d7ab9ecc07b0114f83aaa6c8fe82
ATEGBO <carine.ategbo@epitech.eu>
2022-10-05 17:36:20 +0200

    Add ex 09

---
+++ ex_07/ex_07.php
@@ -0,0 +1,9 @@
+<?php
+function get_angry_dog($nbrepetition){
+    $w="";
+    for ($i= 0;$i< $nbrepetition; $i++)
+    $w= $w."woof";
+    return($w);
+}
+"\n";
+?>
---
+++ ex_09/ex_09.php
@@ -0,0 +1,19 @@
+<?php
+function movieTitle($nbr){
+switch ($nbr){
+  case "3":
+    echo "The Three Brothers";
+    break;
+  case "6":
+    echo "The Sixth Sence";
+    break;
+  case "23":
+    echo "The Number 23";
+    break;
+    case "28":
+    echo "28 Days Later";
+    break;
+  default:
+    echo "I don't know";
+}}    
+?>
